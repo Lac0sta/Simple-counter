3c06c8a94f0b5f284b685874e5598aaf7c0272f8
Alekse1Frolov <aleksei.frolov@customertimes.com>
2024-09-03 23:23:15 +0200

    Added the main functionality of the buttons and configuration in viewDidLoad

--- Simple counter/ViewController.swift
+++ Simple counter/ViewController.swift
@@ -9,25 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var counterNumber: UInt = 0
+    
     @IBOutlet private var counterLabel: UILabel!
     @IBOutlet private var plusButton: UIButton!
     @IBOutlet private var minusButton: UIButton!
     @IBOutlet private var dropCounterButton: UIButton!
     @IBOutlet private var textViewField: UITextView!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        counterLabel.text = "\(counterNumber)"
+        textViewField.isEditable = false
+        textViewField.text = "Action history:\n"
+        textViewField.font = UIFont.boldSystemFont(ofSize: 13)
     }
     
+    @IBAction func plusButtonClicked(_ sender: UIButton) {
+        counterNumber += 1
+        counterLabel.text = "\(counterNumber)"
+    }
     
-    @IBAction func plusButtonClicked(_ sender: UIButton) { }
+    @IBAction func minusButtonClicked(_ sender: UIButton) {
+        counterNumber -= 1
+        counterLabel.text = "\(counterNumber)"
+    }
     
-    @IBAction func minusButtonClicked(_ sender: UIButton) { }
-    
-    @IBAction func dropCounterButtonClicked(_ sender: UIButton) { }
+    @IBAction func dropCounterButtonClicked(_ sender: UIButton) {
+        counterNumber = 0
+        counterLabel.text = "\(counterNumber)"
+    }
     
     
     
