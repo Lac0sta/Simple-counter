f7eef928ee7ed8efc8b00b37c31bd756d9a9cb4e
Alekse1Frolov <aleksei.frolov@customertimes.com>
2024-09-03 22:57:06 +0200

    Outlets for buttons have been added

--- Simple counter/ViewController.swift
+++ Simple counter/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var counterLabel: UILabel!
+    @IBOutlet private var plusButton: UIButton!
+    @IBOutlet private var minusButton: UIButton!
+    @IBOutlet private var dropCounterButton: UIButton!
+    @IBOutlet private var textViewField: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    
+    @IBAction func plusButtonClicked(_ sender: UIButton) { }
+    
+    @IBAction func minusButtonClicked(_ sender: UIButton) { }
+    
+    @IBAction func dropCounterButtonClicked(_ sender: UIButton) { }
+    
+    
+    
 
 }
 
